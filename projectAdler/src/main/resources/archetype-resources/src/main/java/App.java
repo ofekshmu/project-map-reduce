636dad0c145cd1b36207246db386f6de16a904f2
ofek <ofek.shmuel1@gmail.com>
2020-04-13 19:39:22 +0300

    first commit and push

---
+++ projectAdler/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $projectAdler.projectAdler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
