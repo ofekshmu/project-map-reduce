64129a5d3b7ab4052785899b2a7d4e5d111ba6e1
ofek <ofek.shmuel1@gmail.com>
2020-04-14 16:15:25 +0300

    testcommitmaster

--- projectAdler/src/main/resources/archetype-resources/src/main/java/LocalApp.java
+++ projectAdler/src/main/resources/archetype-resources/src/main/java/LocalApp.java
@@ -14,7 +14,7 @@ import com.amazonaws.services.sqs.model.Message;
 
 /**
  * Distributed System Programming : Cloud Computing and Map-Reducce1 - 2020/Spring
- * Assignment 1
+ * Assignment 1!
  */
 public class LocalApp {
 
